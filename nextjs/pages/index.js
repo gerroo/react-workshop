64ea8b129640db6d3602e0340c99b087b46a6a1a
Eren Suner <gerensuner@gmail.com>
2019-10-24 20:42:27 -0400

    x

---
+++ nextjs/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+export default () => {
+  return (
+    <h1>HEYY</h1>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+export default () => {
+  <h1>HEYY</h1>
+}
